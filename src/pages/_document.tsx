8ce51c0722940e906307f21aebb77d8d9420fc56
Your Name <your.email@example.com>
2025-03-17 14:33:06 +0200

    Fix ESLint configuration to disable unescaped-entities rule

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,6 +10,13 @@ export default function Document() {
         <meta name="description" content="Finlern - Finnish Language Learning" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+        
+        {/* iOS Safari compatibility script */}
+        <Script 
+          id="ios-fixes" 
+          strategy="beforeInteractive"
+          src="/ios-compatibility.js"
+        />
       </Head>
       <body>
         <Main />

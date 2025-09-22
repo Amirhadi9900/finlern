8542fd126684540498bc366444ef6eb9704c68f8
Your Name <your.email@example.com>
2025-09-22 17:48:58 +0300

    Implemented detailed error messages and integrated form across English and Violin course pages.

--- src/pages/api/enrollment-email.ts
+++ src/pages/api/enrollment-email.ts
@@ -8,7 +8,7 @@ type Data = {
 
 const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI || 'https://developers.google.com/oauthplayground'; // This should be a URL your app controls, but for getting refresh token, playground is fine
+const REDIRECT_URI = process.env.GOOGLE_REDIRECT_URI;
 const REFRESH_TOKEN = process.env.GOOGLE_REFRESH_TOKEN;
 
 const oAuth2Client = new google.auth.OAuth2(
